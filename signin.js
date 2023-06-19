e207be3939f261ed593adbdeabfc5265877602dc
Greater88 <136064916+Greater88@users.noreply.github.com>
2023-06-19 01:00:19 -0700

    Add files via upload
    Use signin with password

---
+++ signin.js
@@ -0,0 +1,111 @@
+import {
+  Alert,
+  AlertIcon,
+  Box,
+  Button,
+  chakra,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import { useState, useRef } from "react";
+import { supabaseClient } from "../lib/client";
+
+const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const passwordRef = useRef(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [error, setError] = useState(null);
+
+  const submitHandler = async (event) => {
+      event.preventDefault();
+      setIsLoading(true);
+      setError(null);
+      try {
+          const { error } = await supabaseClient.auth.signIn({
+              email: email,
+              password: passwordRef.current?.value
+          });
+          if (error) {
+              setError(error.message);
+          } else {
+              setIsSubmitted(true);
+          }
+      } catch (error) {
+          setError(error.message);
+      } finally {
+          setIsLoading(false);
+      }
+  };
+
+  const changeHandler = (event) => {
+      setEmail(event.target.value);
+  };
+
+  return (
+      <Box minH="100vh" py="12" px={{ base: "4", lg: "8" }} bg="gray.50">
+          <Box maxW="md" mx="auto">
+              <Heading textAlign="center" m="6">
+                  Welcome to Todo App
+              </Heading>
+              {error && (
+                  <Alert status="error" mb="6">
+                      <AlertIcon />
+                      <Text textAlign="center">{error}</Text>
+                  </Alert>
+              )}
+              <Box
+                  py="8"
+                  px={{ base: "4", md: "10" }}
+                  shadow="base"
+                  rounded={{ sm: "lg" }}
+                  bg="white"
+              >Sign In
+                  {isSubmitted ? (
+                      <Heading size="md" textAlign="center" color="gray.600">
+                          Please check {email} for login link
+                      </Heading>
+                  ) : (
+                      <chakra.form onSubmit={submitHandler}>
+                          <Stack spacing="6">
+                              <FormControl id="email">
+                                  <FormLabel>Email address</FormLabel>
+                                  <Input
+                                      name="email"
+                                      type="email"
+                                      autoComplete="email"
+                                      required
+                                      value={email}
+                                      onChange={changeHandler}
+                                  />
+                                  <Input
+                                      name="password"
+                                      type="password"
+                                      autoComplete="password"
+                                      required
+                                      ref={passwordRef}
+                                  />
+                              </FormControl>
+                              <Button
+                                  type="submit"
+                                  colorScheme="blue"
+                                  size="lg"
+                                  fontSize="md"
+                                  isLoading={isLoading}
+                              >
+                                  Sign in
+                              </Button>
+                          </Stack>
+                      </chakra.form>
+                  )}
+              </Box>
+          </Box>
+      </Box>
+  );
+};
+
+export default SignIn;
